956bacebe40c1c7e2ad8334424fde07d2ef9a472
Matthias Kretschmann <m@kretschmann.io>
2017-05-20 01:01:17 +0200

    new build flow, works with Docker

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM ubuntu
 
-RUN apt-get update && apt-get install -y texlive texlive-latex-extra pdftk make
+RUN apt-get update && apt-get install -y texlive texlive-latex-extra pdftk
 
-RUN mkdir -p /usr/src/docs
+RUN mkdir -p /usr/src/whitepaper
 
-VOLUME /usr/src/docs
-WORKDIR /usr/src/docs
+VOLUME /usr/src/whitepaper
+WORKDIR /usr/src/whitepaper
--- bin/build.sh
+++ bin/build.sh
@@ -1,15 +1,38 @@
 #!/usr/bin/env bash
 
+FILENAME='bigchaindb-whitepaper.pdf'
+
 set -x
 
 # remove artifacts
-rm -f dist/*
+rm -f $FILENAME
+
+# Either of those would be sooooo great, and would replace this whole file
+#pandoc -o $FILENAME src/index.tex
+#rubber --pdf --into=dist --texpath=img src/index.tex
 
 # generate pdf
-pdflatex -output-directory=dist -output-format=pdf -interaction=nonstopmode src/index.tex
-pdflatex -output-directory=dist -output-format=pdf -interaction=nonstopmode src/addendum.tex
+cd src/
+pdflatex -interaction=nonstopmode index.tex
+pdflatex -interaction=nonstopmode addendum.tex
 
-bibtex dist/index.aux # Don't bibtex addendum
+bibtex index.aux # Don't bibtex addendum
+
+pdflatex -interaction=nonstopmode index.tex
+pdflatex -interaction=nonstopmode addendum.tex
+
+# needs to be run again to finally make footnotes & bibliography work
+pdflatex -interaction=nonstopmode index.tex
+pdflatex -interaction=nonstopmode addendum.tex
 
 # combine pdflatex output files
-pdftk dist/index.pdf dist/addendum.pdf cat output dist/bigchaindb-whitepaper.pdf
+pdftk index.pdf addendum.pdf cat output $FILENAME
+
+mv bigchaindb-whitepaper.pdf ../$FILENAME
+
+# clean up after ourselves
+rm -f *.aux *.bbl *.blg *.out *.pdf *.log
+
+echo "PDF successfully generated. Whoop whoop."
+
+exit

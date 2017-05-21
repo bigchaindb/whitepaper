28e9f8cc90898b99f96b89701e41a1dd92089738
Matthias Kretschmann <m@kretschmann.io>
2017-05-21 17:54:16 +0200

    rewrite build script with better flow & more checks

--- build.sh
+++ build.sh
@@ -2,36 +2,69 @@
 
 FILENAME='bigchaindb-whitepaper.pdf'
 
-set -x
+# # Either of those would be sooooo great, and would replace this whole file
+# #pandoc -o $FILENAME src/index.tex
+# #rubber --pdf --texpath=src/img src/index.tex
 
-# remove artifacts
-rm -f $FILENAME
+# check that pdflatex is installed and available
+if ! pdflatex_loc="$(type -p "pdflatex")" || [ -z "$pdflatex_loc" ]; then
+	echo "FATAL ERROR: pdflatex not available, please check LaTeX installation and environment"
+	exit
+fi
 
-# Either of those would be sooooo great, and would replace this whole file
-#pandoc -o $FILENAME src/index.tex
-#rubber --pdf --into=dist --texpath=img src/index.tex
+function pdfs {
+    pdflatex -interaction=nonstopmode index.tex
+    pdflatex -interaction=nonstopmode addendum.tex
+}
 
-# generate pdf
-cd src/
-pdflatex -interaction=nonstopmode index.tex
-pdflatex -interaction=nonstopmode addendum.tex
+function pdfs_combine {
+    pdftk index.pdf addendum.pdf cat output $FILENAME
+}
 
-bibtex index.aux # Don't bibtex addendum
+# create or empty the build tmp folder
+if ! [ -d "tmp" ]; then
+	mkdir tmp
+else
+	rm -R -f tmp/*
+fi
 
-pdflatex -interaction=nonstopmode index.tex
-pdflatex -interaction=nonstopmode addendum.tex
+# remove whitepaper in root
+rm -R -f $FILENAME
 
-# needs to be run again to finally make footnotes & bibliography work
-pdflatex -interaction=nonstopmode index.tex
-pdflatex -interaction=nonstopmode addendum.tex
+# copy src to build dir and go in
+cp src/*.* tmp/
+cp -R src/img tmp/
+cd tmp
+
+# build, first pass
+pdfs
+
+# bibtext the output
+bibtex index.aux
+
+# build, second pass, needed for page numbering
+pdfs
+
+# build, third pass, needed for bibliography
+pdfs
+
+# check the intermediate PDFs were built and exist
+if ! [ -f "index.pdf" ] || ! [ -f "addendum.pdf" ]; then
+	echo "FATAL ERROR: could not construct PDF files, check tex sources for errors"
+	exit
+fi
 
 # combine pdflatex output files
-pdftk index.pdf addendum.pdf cat output $FILENAME
+pdfs_combine
 
-mv bigchaindb-whitepaper.pdf ../$FILENAME
+# check for final pdf file being there
+if ! [ -f $FILENAME ]; then
+    echo "FATAL ERROR: could not construct final PDF file, check tex sources or pdftk output for errors"
+	exit
+fi
 
-# clean up after ourselves
-rm -f *.aux *.bbl *.blg *.out *.pdf *.log
+# copy bigchaindb-whitepaper.pdf to root directory
+cp $FILENAME ../$FILENAME
 
 echo "PDF successfully generated. Whoop whoop."
 
